1e72a2dbc05913503aa7bd6ad8cf40c6e8f24c1d
yashaka <yashaka@gmail.com>
2021-11-10 14:55:20 +0200

    added file upload support via 3rd party plugiin

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,8 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+import 'cypress-file-upload'
+
 Cypress.Commands.add(
   'setValue', 
   { prevSubject: 'element'}, 
